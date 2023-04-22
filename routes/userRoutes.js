a63d88100e9b903ed4d53b55461c643b0422fc35
Greg <greg.renfro93@gmail.com>
2023-04-21 22:43:53 -0700

    created pg database and made connection pool

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,7 +3,7 @@ const { Router } = require("express")
 const router = Router();
 
 router.get('/', (req, res) => {
-    res.send("using api route")
+    res.send("using api route for users")
 })
 
 module.exports = router;
